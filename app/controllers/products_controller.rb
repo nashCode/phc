083589049b041c997e3229c69ae2e495fc691ea0
Jose Manuel Dominguez <josemanueljd@hotmail.com>
2017-09-05 19:18:13 -0500

    crea la ruta prodcuts/:id

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,8 +17,14 @@ class ProductsController < ApplicationController
 
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
   private
   	def product_params
   		params.require(:product).permit(:name, :url, :description)
   	end
+  
+
 end
